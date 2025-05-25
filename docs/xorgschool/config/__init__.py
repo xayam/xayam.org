e5f9930da379901ff3923853b9aa3b3a47effa32
xayam <xayam@yandex.ru>
2025-05-25 21:01:53 +0300

    v0.2

--- docs/xorgschool/config/__init__.py
+++ docs/xorgschool/config/__init__.py
@@ -5,3 +5,8 @@ from __secret__ import *
 # Название школы
 XORGSHOOL_NAME = "Школа КСАЯМа"
 
+# Включение/отключение телеграм-бота портфолио основателя
+TELEGRAM_XPORTFOLIOS_ENABLE = True
+
+# Включение/отключение сайта школы
+TELEGRAM_XORGSHOOL_ENABLE = True
