5095576a288f8a2d49eebc990bf82e505ecd9b41
xayam <xayam@yandex.ru>
2025-05-25 20:31:20 +0300

    v0.0 init commit

---
+++ docs/xorgschool/config/init.py
@@ -0,0 +1,14 @@
+
+# Название школы
+XORGSHOOL_NAME = "НАЗВАНИЕ_ВАШЕЙ_ШКОЛЫ"
+
+# Портфолио основателя
+TELEGRAM_XPORTFOLIOS_BOT_ID = None # установите свое значение, тип int
+TELEGRAM_XPORTFOLIOS_BOT_API_KEY = \
+    str(TELEGRAM_XPORTFOLIOS_BOT_ID) + ":" + "YOU_API_KEY"
+
+# Бот, реализующий все этапы общения между школой и учениками
+TELEGRAM_XORGSCHOOL_BOT_ID = None # установите свое значение, тип int
+TELEGRAM_XORGSCHOOL_BOT_API_KEY = \
+    str(TELEGRAM_XORGSCHOOL_BOT_ID) + ":" + "YOU_API_KEY"
+
